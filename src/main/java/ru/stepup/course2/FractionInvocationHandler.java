a51cf8e6faf2da145e2fc45654b684d4cdef12f3
evgengit2007 <vend111@yandex.ru>
2024-04-17 23:57:24 +0700

    версия без тестов

--- src/main/java/ru/stepup/course2/FractionInvocationHandler.java
+++ src/main/java/ru/stepup/course2/FractionInvocationHandler.java
@@ -2,17 +2,42 @@ package ru.stepup.course2;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 
 public class FractionInvocationHandler implements InvocationHandler {
-    private Fraction fraction;
+    private Object object;
+    private HashMap<String, Double> hashMap;
 
-    public FractionInvocationHandler(Fraction fraction) {
-        this.fraction = fraction;
+    public FractionInvocationHandler(Object object) {
+        this.object = object;
+        this.hashMap = new HashMap<>();
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println("Before: ");
-        return method.invoke(proxy, args);
+        double result;
+        String key = "keyHashMap";
+//        System.out.println("Before: ");
+        Method m = object.getClass().getMethod(method.getName(), method.getParameterTypes());
+//        System.out.println(m.getName());
+        if (m.isAnnotationPresent(Cache.class)) {
+            if (hashMap.containsKey(key)) {
+                result = hashMap.get(key);
+                System.out.println(result);
+                return result;
+            }
+            result = (Double) m.invoke(object, args);
+            hashMap.put(key, result);
+            System.out.println(result);
+            return result;
+        }
+        if (m.isAnnotationPresent(Mutator.class)) {
+            if (hashMap.containsKey(key)) {
+                hashMap.remove(key);
+            }
+            System.out.println(hashMap.get(key));
+            return m.invoke(object, args);
+        }
+        return m.invoke(object, args);
     }
 }
--- src/main/java/ru/stepup/course2/Utils.java
+++ src/main/java/ru/stepup/course2/Utils.java
@@ -1,25 +1,16 @@
 package ru.stepup.course2;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-public class Utils implements Cache, Mutator{
+public class Utils {
     public static Fractionable cache(Fraction fraction) {
-        System.out.println("Utils, cache");
+//        System.out.println("Utils, cache");
         ClassLoader classLoader = Fraction.class.getClassLoader();
         Class[] interfaces = fraction.getClass().getInterfaces();
-        for (Class i: interfaces) {
-            System.out.println(i.getDeclaredMethods());
-        }
         Fractionable proxyFraction = (Fractionable) Proxy.newProxyInstance(classLoader,
                 interfaces,
                 new FractionInvocationHandler(fraction));
         return proxyFraction;
     }
 
-    @Override
-    public Class<? extends Annotation> annotationType() {
-        return null;
-    }
 }
